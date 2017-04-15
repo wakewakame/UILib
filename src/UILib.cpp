4e9c638dbf9a2b81601b91e61f13dd306be9ecae
ta <0214.taiki@gmail.com>
2017-04-16 00:31:33 +0900

    改善

--- src/UILib.cpp
+++ src/UILib.cpp
@@ -1,16 +1,39 @@
 #include "UILib.h"
 
-void UILib::init(int *argc, char *argv[]) {
+void UILib::resize() {
+	if (f_exit) return;
+	//ウィンドウ全体をビューポートにする
+	glViewport(0, 0, win.size.x, win.size.y);
+
+	//変換行列の初期化
+	glLoadIdentity();
+
+	//スクリーン上の表示領域をビューポートの大きさに比例させる
+	glOrtho(0.0, (double)win.size.x, (double)win.size.y, 0.0, -1.0, 1.0);
+}
+
+bool UILib::error(bool val) {
+	if (val) {
+		exit();
+		f_exit = 1;
+	}
+	return val;
+}
+
+void UILib::init() {
 	//変数初期化
 	f_exit = 0;
 
 	//GLFW初期化
-	if (glfwInit() == GL_FALSE) {
-		f_exit = 1;
-		return;
-	}
-	
-	// ウィンドウ生成
+	if (error(glfwInit() == GL_FALSE)) return;
+
+	//OpenGL Version 3.2 Core Profile を選択する
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	//glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//ウィンドウ生成
 	GLhwnd = glfwCreateWindow
 	(
 		win.size.x,
@@ -24,34 +47,27 @@ void UILib::init(int *argc, char *argv[]) {
 	win.hwnd = (HWND)GLhwnd;
 	
 	//ウィンドウが生成されていなければ終了
-	if (win.hwnd == NULL)
-	{
-		f_exit = 1;
-		return;
-	}
+	if (error(win.hwnd == NULL)) return;
 	
 	//生成したウィンドウをOpenGLの処理対象にする
 	glfwMakeContextCurrent(GLhwnd);
+
+	//GLEW初期化
+	glewExperimental = GL_TRUE;
+	if (error(glewInit() != GLEW_OK)) return;
 	
 	//ブレンドモード有効化
-	glEnable(GL_BLEND); //ブレンドモード有効化
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //アルファブレンド適用
+	glEnable(GL_BLEND);
+
+	//アルファブレンド適用
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	//ウィンドウとビューポートの同期
 	resize();
+
+	//垂直同期のタイミングを待つ
+	glfwSwapInterval(1);
 	return;
-}
-
-void UILib::resize() {
-	if (f_exit) return;
-	//ウィンドウ全体をビューポートにする
-	glViewport(0, 0, win.size.x, win.size.y);
-
-	//変換行列の初期化
-	glLoadIdentity();
-
-	//スクリーン上の表示領域をビューポートの大きさに比例させる
-	glOrtho(0.0, (double)win.size.x, (double)win.size.y, 0.0, -1.0, 1.0);
 }
 
 void UILib::loop() {
@@ -59,47 +75,46 @@ void UILib::loop() {
 	//ウィンドウが開いている間繰り返す
 	while (glfwWindowShouldClose(GLhwnd) == GL_FALSE)
 	{
-		//ウィンドウを消去する
+		//イベントが発生するまで待機
+		//glfwWaitEventsTimeout(0.7);
+		glfwPollEvents();
+		//glfwWaitEvents();
+		//SetTimer(win.hwnd, NULL, 1000.0 / win.fps, NULL);
+
+		//画面初期化
+		glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//ウィンドウの背景色設定
-
-		///debug///
-		glClearColor(1.0, 1.0, 1.0, 1.0);
-		///debug///
-
-		//描画処理
-		
-		///debug///
-		static int a = 50;
-		a = (a + 1) % 100;
-		glClear(GL_COLOR_BUFFER_BIT);
-		glColor4d(0.0, 1.0, 1.0, 1.0);
-		glBegin(GL_QUADS);
-		glVertex2d(0.0, 0.0);
-		glVertex2d(100.0, 0.0);
-		glVertex2d(100.0, 100.0);
-		glVertex2d(0.0, 100.0);
-		glEnd();
-		glColor4d(1.0, 0.0, 0.0, 0.5);
-		glBegin(GL_QUADS);
-		glVertex2d(10.0 + (double)a, 10.0);
-		glVertex2d(90.0 + (double)a, 10.0);
-		glVertex2d(90.0 + (double)a, 90.0);
-		glVertex2d(10.0 + (double)a, 90.0);
-		glEnd();
-		glFlush();
-		///debug///
+		//描画
+		render();
 
 		//カラーバッファを入れ替える
 		glfwSwapBuffers(GLhwnd);
-
-		//イベントが発生するまで待機
-		glfwWaitEvents();
 	}
 }
 
+void UILib::render() {
+	static int a = 50;
+	a = (a + 1) % 100;
+	glColor4d(0.0, 1.0, 1.0, 1.0);
+	glBegin(GL_QUADS);
+	glVertex2d(0.0, 0.0);
+	glVertex2d(100.0, 0.0);
+	glVertex2d(100.0, 100.0);
+	glVertex2d(0.0, 100.0);
+	glEnd();
+	glColor4d(1.0, 0.0, 0.0, 0.5);
+	glBegin(GL_QUADS);
+	glVertex2d(10.0 + (double)a, 10.0);
+	glVertex2d(90.0 + (double)a, 10.0);
+	glVertex2d(90.0 + (double)a, 90.0);
+	glVertex2d(10.0 + (double)a, 90.0);
+	glEnd();
+	glFlush();
+}
+
 void UILib::exit() {
+	if (f_exit) return;
 	//GLFWの終了
 	glfwTerminate();
 }
--- src/UILib.h
+++ src/UILib.h
@@ -4,10 +4,11 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 #include <iostream>
+#include <Windows.h>
 #include "win_info.h"
 
 class UILib {
-public:
+private:
 //変数宣言
 	//ウィンドウ情報保持変数初期化
 	WINDOW_INFO win;
@@ -18,12 +19,19 @@ public:
 		bool f_exit;
 
 //関数宣言
-	//初期化関数
-	void init(int *argc, char *argv[]);
 	//ウィンドウとビューポートを同期
 	void resize();
+	//エラー発生時処理関数
+	bool error(bool val);
+
+public:
+//関数宣言
+	//初期化関数
+	void init();
 	//ループ関数
 	void loop();
+	//描画関数
+	void render();
 	//終了時処理
 	void exit();
 };
