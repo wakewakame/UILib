c392401ec55757f6b7e78c489b438f0027245a01
ta <0214.taiki@gmail.com>
2017-04-17 00:44:41 +0900

    コメント追加

--- src/UILib.cpp
+++ src/UILib.cpp
@@ -31,7 +31,7 @@ void UILib::init() {
 	//glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 	//ウィンドウ生成
